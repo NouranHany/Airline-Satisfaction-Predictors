a9e4627d2dc3804533f528bc758ae82fe8974850
MUHAMMAD SAAD <61880555+Muhammad-saad-2000@users.noreply.github.com>
2023-05-14 20:24:06 +0000

    Added Feature importance

--- ModelPipelines/RandomForest/RandomForest.py
+++ ModelPipelines/RandomForest/RandomForest.py
@@ -1,7 +1,9 @@
 # pylint: disable-all
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import Bucketizer, StringIndexer
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 spark = SparkSession.builder \
     .appName("Naive Bayse") \
@@ -48,3 +50,25 @@ def process_dataframe(df, string_indexers={}, bucketizers={}):
     df = df.withColumn(feature, df[feature].cast("integer"))
   
   return df, string_indexers, bucketizers
+
+def importance_bar_plot(model, features):
+  # Get feature importances from model
+  importances = model.featureImportances.toArray()
+  # Create pandas dataframe of features and importances
+  feature_df = pd.DataFrame({"feature": features, "importance": importances})
+  # Sort features by importance
+  feature_df = feature_df.sort_values("importance", ascending=True)
+  # Set dpi and style of charts
+  plt.rcParams["figure.dpi"] = 300
+  plt.style.use("dark_background")
+  cmap = plt.get_cmap("magma")
+  colors = [cmap(i) for i in np.linspace(0, 1, len(feature_df))]
+  # Create the bar plot
+  fig, ax = plt.subplots(figsize=(8, 8))
+  ax.barh(feature_df["feature"], feature_df["importance"], color=colors)
+  # Set the labels and title
+  ax.set_xlabel("Importance")
+  ax.set_ylabel("Feature")
+  ax.set_title("Feature Importance")
+  # Show the plot
+  plt.show()
